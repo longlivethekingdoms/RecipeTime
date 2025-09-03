e43dc6a239d7f11518f23d89a47a978d685c178d
Student <Student@STUDENT-PC-01>
2025-09-03 12:56:43 +0900

    lobbycontroller와 pom.xml 코드 추가
    LC는 autowired 및 링크 추가
    pom.xml은 자바 버전 11로 변경

--- RecipeTime/src/main/java/com/recipetime/find/Controller/LobbyController.java
+++ RecipeTime/src/main/java/com/recipetime/find/Controller/LobbyController.java
@@ -1,5 +1,19 @@
 package com.recipetime.find.Controller;
 
-public class LobbyController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import com.recipetime.find.Service.UserService;
+
+@Controller
+public class LobbyController {
+	
+	@Autowired
+	UserService userService;
+	
+	@GetMapping("/")
+	String index() {
+		return "index";
+	}
 }
