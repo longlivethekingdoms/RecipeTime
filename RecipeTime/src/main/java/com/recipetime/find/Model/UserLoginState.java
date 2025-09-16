b9718a08c10d0cc27978454153537d70cff949ae
Student <Student@STUDENT-PC-01>
2025-09-16 17:53:22 +0900

    유저로그인스테이트 추가

--- RecipeTime/src/main/java/com/recipetime/find/Model/UserLoginState.java
+++ RecipeTime/src/main/java/com/recipetime/find/Model/UserLoginState.java
@@ -1,5 +1,16 @@
 package com.recipetime.find.Model;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 public class UserLoginState {
+	private String userid;
+	private int wrongnum;
+	private int blocklogin;
+	@DateTimeFormat(pattern="yyyy-MM-dd")
+	@JsonFormat(pattern="yyyy-MM-dd")
+	private LocalDate blockdate;
+
 	
 }
