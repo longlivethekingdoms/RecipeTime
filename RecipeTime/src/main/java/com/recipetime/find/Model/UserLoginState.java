99ec5c9789eac5f7bd6aa1cbb41c44eaef4733c0
dddvp <dddvp@192.168.243.218>
2025-09-16 22:09:41 +0900

    유저로그인스테이트 모델 완성

--- RecipeTime/src/main/java/com/recipetime/find/Model/UserLoginState.java
+++ RecipeTime/src/main/java/com/recipetime/find/Model/UserLoginState.java
@@ -1,5 +1,7 @@
 package com.recipetime.find.Model;
 
+import java.time.LocalDate;
+
 import org.springframework.format.annotation.DateTimeFormat;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -11,6 +13,31 @@ public class UserLoginState {
 	@DateTimeFormat(pattern="yyyy-MM-dd")
 	@JsonFormat(pattern="yyyy-MM-dd")
 	private LocalDate blockdate;
+	
+	public String getUserid() {
+		return userid;
+	}
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+	public int getWrongnum() {
+		return wrongnum;
+	}
+	public void setWrongnum(int wrongnum) {
+		this.wrongnum = wrongnum;
+	}
+	public int getBlocklogin() {
+		return blocklogin;
+	}
+	public void setBlocklogin(int blocklogin) {
+		this.blocklogin = blocklogin;
+	}
+	public LocalDate getBlockdate() {
+		return blockdate;
+	}
+	public void setBlockdate(LocalDate blockdate) {
+		this.blockdate = blockdate;
+	}
 
 	
 }
