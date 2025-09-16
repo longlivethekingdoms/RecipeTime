bc8cd3bc28fd60ffd62fee834928ac517c63e5e7
Student <Student@STUDENT-PC-01>
2025-09-16 16:42:09 +0900

    categoryoption 푸쉬

--- RecipeTime/src/main/java/com/recipetime/find/Model/CategoryOption.java
+++ RecipeTime/src/main/java/com/recipetime/find/Model/CategoryOption.java
@@ -3,5 +3,26 @@ package com.recipetime.find.Model;
 public class CategoryOption {
 	private int optionid;
 	private int itemid;
-	private String optionname; 
+	private String optionname;
+	
+	public int getOptionid() {
+		return optionid;
+	}
+	public void setOptionid(int optionid) {
+		this.optionid = optionid;
+	}
+	public int getItemid() {
+		return itemid;
+	}
+	public void setItemid(int itemid) {
+		this.itemid = itemid;
+	}
+	public String getOptionname() {
+		return optionname;
+	}
+	public void setOptionname(String optionname) {
+		this.optionname = optionname;
+	}
+	
+	
 }
